6f42734e3c59a1a126d216b05fc6e965424590be
vansh-tiwari <tiwari1998@hotmail.com>
2020-02-15 22:26:35 +0530

    delete

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -8,7 +8,7 @@ class LinkedList:
         self.head = None
 
     #Helps in adding new values
-    def append(self, value):
+    def insertFirst(self, value):
         newNode = Node(value)
         #if self.head==None:
         #    self.head = newNode
@@ -17,15 +17,33 @@ class LinkedList:
         self.head = newNode
 
     #Helps in adding value after specific index
-    def appendAfter(self, index, value):
+    def insertAfter(self, index, value):
         newNode = Node(value)
         temp = self.head
+        if self.head == None:
+            self.head = newNode
+            return
+
         while index>1:
             temp = temp.next
             index-=1
         newNode.next = temp.next
         temp.next = newNode
 
+    #Adding value at last of list
+    def insertLast(self, value):
+        newNode = Node(value)
+        temp = self.head
+
+        if self.head == None:
+            self.head = newNode
+            return
+
+        while temp.next!=None:
+            temp = temp.next
+        temp.next = newNode
+
+    #Reversing list
     def reverse(self):
         prev = None
         cur = self.head
@@ -35,6 +53,24 @@ class LinkedList:
             prev = cur
             cur = nextNode
         self.head = prev
+
+    #Delete first element from list
+    def deleteFirst(self):
+        if self.head == None:
+            return
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+
+    def deleteValue(self,value):
+        if self.head == None:
+            return
+        temp = self.head
+        while temp.next.value != value:
+            temp = temp.next
+        prev = temp.next
+        temp.next = temp.next.next
+        prev.next=None
 
     #Helps in printing values of List
     def println(self):
--- LinkedList/test.py
+++ LinkedList/test.py
@@ -1,10 +1,18 @@
 from LinkedList import *
 ll = LinkedList()
-n = int(input())
-for x in range(n):
-    ll.append(x)
 
-# ll.appendAfter(3, 101)
+#No. to input in list
+n = int(input())
+
+#Inserting in order
+for x in range(n,0,-1):
+    ll.insertFirst(x)
+
+ll.insertAfter(4, 1229)
+ll.println()
+ll.insertLast(25000)
 ll.println()
 ll.reverse()
+ll.println()
+ll.deleteFirst()
 ll.println()
