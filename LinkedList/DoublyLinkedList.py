4375b744d10017d860b291edf4538b9f2c63313c
vansh-tiwari <tiwari1998@hotmail.com>
2020-02-17 23:19:36 +0530

    Dll functions

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -1,11 +1,33 @@
-class Node:
+class Dnode:
     def __init__(self, value):
         self.value = value
         self.next = None
-        self.next = None
+        self.prev = None
 
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
     
-    
+    def insertFirst(self, value):
+        newNode = Dnode(value)
+        newNode.next = self.head
+        newNode.prev = None
+        if self.head != None:
+            self.head.prev = newNode
+        self.head = newNode
+
+    def println(self):
+        if self.head == None:
+            return
+        temp = self.head
+        while temp!=None:
+            last = temp
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+        while last!=None:
+            print(last.value, end=" ")
+            last = last.prev
+        print()
+        
--- LinkedList/test.py
+++ LinkedList/test.py
@@ -8,15 +8,13 @@ dll = DoublyLinkedList()
 n = int(input())
 
 #Inserting in order
-for x in range(n,0,-1):
-    ll.insertFirst(x)
+for x in range(n):
+    dll.insertFirst(x)
 
 # ll.insertAfter(4, 1229)
-# ll.println()
 # ll.insertLast(25000)
-# ll.println()
 # ll.reverse()
-# ll.println()
 # ll.deleteFirst()
-ll.println()
-ll.length()
+# ll.println()
+# ll.length()
+dll.println()
